bf1b1ffca8a797a8a6571d25c15cb3f56acf60d0
Quốc Việt <viet6460@gmail.com>
2026-09-20 14:12:12 +0700

    Tuan01/Lab01: Làm quen với ngôn ngữ C# (Nhóm)

---
+++ lab_01/buoi09/LietKeChan.cs
@@ -0,0 +1,26 @@
+namespace NMLT.Buoi09
+{
+    public static class LietKeChan
+    {
+        public static void Main()
+        {
+            Console.Write("Moi ban nhap so luong phan tu: ");
+            int n = int.Parse(Console.ReadLine()!);
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Phan tu {i}: ");
+                a[i] = int.Parse(Console.ReadLine()!);
+            }
+
+            Console.Write("Cac phan tu chan trong mang: ");
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (a[i] % 2 == 0)
+                    Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
