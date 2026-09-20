5b16cfec0f6361a53841d6a8125e7aae747333ff
Quốc Việt <viet6460@gmail.com>
2026-09-20 17:27:55 +0700

    Tuan01/Lab01: Làm quen với ngôn ngữ C# (Nhóm)
    Implement a program to find the longest positive segment in an array of numbers.

---
+++ lab_01/buoi10/DoanDuongDaiNhat.cs
@@ -0,0 +1,60 @@
+namespace NMLT.Buoi10
+{
+    public static class DoanDuongDaiNhat
+    {
+        public static void Main()
+        {
+            // Nhap day so
+            Console.Write("Moi ban nhap so luong phan tu: ");
+            int n = int.Parse(Console.ReadLine()!);
+
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Phan tu {i}: ");
+                a[i] = double.Parse(Console.ReadLine()!);
+            }
+
+            Console.Write($"Day so co {n} phan tu:");
+            for (int i = 0; i < n; i++)
+                Console.Write(" " + a[i]);
+            Console.WriteLine();
+
+            // Tim doan duong dai nhat
+            int iKQ = -1, jKQ = -1;
+            int idx = 0;
+            while (idx < n)
+            {
+                if (a[idx] > 0)
+                {
+                    int j = idx;
+                    while (j < n && a[j] > 0)
+                        j++;
+                    // doan duong la [idx, j-1]
+                    if ((j - 1 - idx) > (jKQ - iKQ))
+                    {
+                        iKQ = idx;
+                        jKQ = j - 1;
+                    }
+                    idx = j;
+                }
+                else
+                {
+                    idx++;
+                }
+            }
+
+            if (iKQ == -1)
+            {
+                Console.WriteLine("Khong co doan duong nao trong day.");
+            }
+            else
+            {
+                Console.Write($"Doan duong [{iKQ}, {jKQ}] dai nhat:");
+                for (int i = iKQ; i <= jKQ; i++)
+                    Console.Write(" " + a[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
