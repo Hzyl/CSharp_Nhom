71395b67acca968416aa3e5d0b166047b3b0703d
Quốc Việt <viet6460@gmail.com>
2026-09-20 13:29:46 +0700

    Refactor date calculation to use DateTime class

--- lab_01/buoi07/NgayTruoc.cs
+++ lab_01/buoi07/NgayTruoc.cs
@@ -11,25 +11,8 @@ namespace NMLT.Buoi07
             Console.Write("Nhap nam: ");
             int nam = int.Parse(Console.ReadLine()!);
 
-            int ngayTruoc, thangTruoc, namTruoc;
-
-            if (ngay > 1)
-            {
-                ngayTruoc = ngay - 1;
-                thangTruoc = thang;
-                namTruoc = nam;
-            }
-            else
-            {
-                thangTruoc = thang > 1 ? thang - 1 : 12;
-                namTruoc = thang > 1 ? nam : nam - 1;
-
-                int[] soNgay = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-                bool namNhuan = (namTruoc % 4 == 0 && namTruoc % 100 != 0) || (namTruoc % 400 == 0);
-                ngayTruoc = (thangTruoc == 2 && namNhuan) ? 29 : soNgay[thangTruoc - 1];
-            }
-
-            Console.WriteLine($"Ngay truoc ngay {ngay}/{thang}/{nam} la ngay {ngayTruoc}/{thangTruoc}/{namTruoc}.");
+            DateTime d = new DateTime(nam, thang, ngay).AddDays(-1);
+            Console.WriteLine($"Ngay truoc ngay {ngay}/{thang}/{nam} la ngay {d.Day}/{d.Month}/{d.Year}.");
         }
     }
 }
